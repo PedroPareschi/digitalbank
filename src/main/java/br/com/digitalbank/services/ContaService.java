b873f1b98542b68b2367236662875797a9f47925
pedropareschi <pedropareschi15@gmail.com>
2022-06-16 22:05:39 -0300

    Usuário só recupera ele mesmo + lidando com exceptions

--- src/main/java/br/com/digitalbank/services/ContaService.java
+++ src/main/java/br/com/digitalbank/services/ContaService.java
@@ -1,8 +1,12 @@
 package br.com.digitalbank.services;
 
 import br.com.digitalbank.domain.Conta;
+import br.com.digitalbank.domain.enums.Perfil;
 import br.com.digitalbank.dtos.ContaDTO;
 import br.com.digitalbank.repositories.ContaRepository;
+import br.com.digitalbank.security.UserSS;
+import br.com.digitalbank.services.exceptions.AuthorizationException;
+import br.com.digitalbank.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -26,6 +30,10 @@ public class ContaService {
     }
 
     public Conta find(Integer id) {
+        UserSS user = UserService.authenticated();
+        if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+            throw new AuthorizationException("Acesso negado");
+        }
         Optional<Conta> conta = repository.findById(id);
         return conta.orElseThrow(() -> new ObjectNotFoundException("Conta não encontrado! Id: " + id));
     }
